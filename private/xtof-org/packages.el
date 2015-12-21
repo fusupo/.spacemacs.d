3dde4d63be022b456768160d603d86cbd61a2917
Marc Christophe <fusupo@gmail.com>
2015-12-20 21:29:46 -0800

    habit capture template helper fn

--- private/xtof-org/packages.el
+++ private/xtof-org/packages.el
@@ -620,10 +620,25 @@
       :Effort: %^{effort|1:00|0:05|0:15|0:30|2:00|3:00|4:00}
       :END:
       %?")
+
+(defun xtof/create-recurring-timestamp ()
+  "foo"
+  (interactive)
+  (let ((fs "%Y-%m-%d %a %H:%M")
+        (date (org-read-date nil t nil "input base date"))
+        (recurrence-type (completing-read "input recurrence type:" (list "+" "++" ".+") nil t nil nil ".+"))
+        (recurrence-rate-min (read-from-minibuffer "input recurrence rate min:" nil nil nil nil nil nil))
+        (recurrence-rate-max (read-from-minibuffer "input recurrence rate nax:" "1" nil nil nil "1" nil))
+        (recurrence-interval (completing-read "input recurrence interval:" (list "y" "w" "m" "d" "h") nil t nil nil "d")))
+    (format "<%s %s%s>"
+            (format-time-string fs date)
+            recurrence-type
+            (cond ((string= "" recurrence-rate-min) (concat recurrence-rate-max recurrence-interval))
+                  (t (concat recurrence-rate-min recurrence-interval "/" recurrence-rate-max recurrence-interval))))))
 (defvar
   xtof/org-capture-habit-template
   "* TODO %^{Habit} %^G 
-  SCHEDULED: %^{schedule}T %^{repeat|.+1d|.+1d/2d|.+2d/4d|.+7|.+1w/2w|}
+  SCHEDULED: %(xtof/create-recurring-timestamp)
   :PROPERTIES:
   :STYLE: habit
   :Effort: %^{effort|1:00|0:05|0:15|0:30|2:00|3:00|4:00}
