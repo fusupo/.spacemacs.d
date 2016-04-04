cd3863f095f6f49c3e6b35e24617436dad33082d
Marc Christophe <m@xtof.net>
2016-04-04 10:23:37 -0700

    adding share buttons to posts(not quite functional)

--- init.el
+++ init.el
@@ -376,7 +376,6 @@ layers configuration. You are free to put any user code."
 
   (defun xtof/org-export-disqus-wiget ()
     (concat
-     "<p></p>"
      "<div id='disqus_thread'></div>"
      "<script>"
      ;; /**
@@ -444,6 +443,12 @@ layers configuration. You are free to put any user code."
                     "<script type='text/javascript' src='js/main.js'></script>"
                     "</div>"))
           (t (concat  "<div>"
+                      "<a href='https://twitter.com/share' class='twitter-share-button' data-via='fusupo' data-size='large'>Tweet</a>"
+                      "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
+                      "<a class='facebook' href='#'"
+                      "onclick=\"window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436'); return false;\">"
+                      "<span class='icon-facebook-rect'>Share</span>"
+                      "</a>"
                       (xtof/org-export-disqus-wiget) 
                       (xtof/org-export-footer)                    
                       (xtof/org-export-google-analytics-widget)
