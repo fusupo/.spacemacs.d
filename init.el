a35e6cc83d766d83da9b8891670ece9c1e7cb9a6
Marc Christophe <m@xtof.net>
2016-04-10 15:50:36 -0700

    snippets and other tooltip enhancements

--- init.el
+++ init.el
@@ -23,7 +23,9 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     auto-completion
+     (auto-completion :variables
+                      auto-completion-enable-help-tooltip t
+                      auto-completion-enable-snippets-in-popup t)
      clojure
      emacs-lisp
      ;;finance
