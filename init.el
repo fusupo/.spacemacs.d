bcff0ec553b3d7d6005168af3a2c7b87f42e37c9
Marc Christophe <fusupo@gmail.com>
2016-08-20 14:30:00 -0700

    moves 512-banner.txt

--- init.el
+++ init.el
@@ -89,7 +89,7 @@ values."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner 512
+   dotspacemacs-startup-banner '~/.spacemacs.d/512-banner.txt'
    ;; List of items to show in the startup buffer. If nil it is disabled.
    ;; Possible values are: `recents' `bookmarks' `projects'.
    ;; (default '(recents projects))
